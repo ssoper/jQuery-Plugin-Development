97e086b0b36d2d66a49dacbe78b7e313486ee398
ssoper <sean.soper@gmail.com>
2010-12-28 15:39:13 -0500

    adding second example

---
+++ Decimate/decimate.js
@@ -0,0 +1,8 @@
+(function($) {
+  $.fn.decimate = function() {
+    var num = Math.ceil(this.children.length / 10);
+    for(var index = 0; index < num; index++) {
+      this.children(':eq(' + index + ')').remove();
+    }
+  };
+})(jQuery);
